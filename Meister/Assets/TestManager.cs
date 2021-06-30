352b6547b3d63c03a31615727f197018201b0651
axn1007 <axn1007@naver.com>
2021-06-30 09:26:07 +0900

    .

---
+++ Meister/Assets/TestManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    public GameObject head;
+    public Transform targetPos;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnClickButton()
+    {
+        head.transform.position = targetPos.position;
+    }
+}
