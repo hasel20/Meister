e3c0213d72d773be8521a23659c4943a718124be
axn1007 <axn1007@naver.com>
2021-06-30 09:53:31 +0900

    .

---
+++ Meister/Assets/Kyee/Scripts/Destroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Meister/Assets/Kyee/Scripts/PartsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsManager : MonoBehaviour
+{
+
+    float createTime = 2;
+    float currTime;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
